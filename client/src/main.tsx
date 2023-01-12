be88b6b9e778eefe55b024e7bd01332a17d74f0a
Meganide <renashassan@hotmail.com>
2023-01-12 16:03:23 +0100

    set up tailwind

--- client/src/main.tsx
+++ client/src/main.tsx
@@ -1,7 +1,17 @@
 import React from 'react'
 import ReactDOM from 'react-dom/client'
 import App from './App'
-import './index.css'
+
+// react
+// nodejs
+// express
+// tailwind
+// prisma
+// nextauth
+// react query
+// axios
+// vite
+// typescript node
 
 ReactDOM.createRoot(document.getElementById('root') as HTMLElement).render(
   <React.StrictMode>
