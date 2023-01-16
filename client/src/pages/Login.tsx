b1c3471d8468c7acfaddbac50ef57ce3deeb044d
Meganide <renashassan@hotmail.com>
2023-01-16 06:25:07 +0100

    changed height to min height to allow scrolling

--- client/src/pages/Login.tsx
+++ client/src/pages/Login.tsx
@@ -6,15 +6,15 @@ function Login() {
   });
 
   return (
-    <section className="flex h-screen items-center justify-center bg-blue-300 p-3 dark:bg-main-dark">
+    <section className="flex min-h-screen items-center justify-center bg-blue-100 p-3 dark:bg-main-dark">
       <section className="flex h-full max-h-[80%] w-full max-w-7xl overflow-hidden rounded-lg">
         {isDesktopOrLaptop && (
-          <aside className="flex h-full flex-1 items-center justify-center bg-login bg-cover bg-center dark:bg-loginDark"></aside>
+          <aside className="flex flex-1 items-center justify-center bg-login bg-cover bg-center dark:bg-loginDark"></aside>
         )}
-        <article className="flex h-full flex-1 flex-col items-center justify-center bg-sky-100 p-7 dark:bg-zinc-800">
-          <article className="flex items-center justify-center gap-5 mb-16">
+        <article className="flex h-full flex-1 flex-col items-center justify-center bg-sky-100 py-16 px-4 dark:bg-zinc-800">
+          <article className="mb-16 flex items-center justify-center gap-5">
             <img className="h-[40px]" src="images/icons/icon.png" alt="icon" />
-            <h1 className="text-3xl dark:text-white">Tweeter</h1>
+            <h1 className="text-2xl dark:text-white md:text-3xl">Tweeter</h1>
           </article>
           <h2 className="mb-5 w-full text-xl dark:text-white sm:max-w-[75%]">Sign in</h2>
           <form action="" className="flex w-full flex-col gap-4 sm:max-w-[75%] sm:p-0">
