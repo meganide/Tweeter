994f7fcdc058513c61df5549df878a2e1db08ab4
Meganide <renashassan@hotmail.com>
2023-01-16 06:02:39 +0100

    added icon

--- client/src/pages/Login.tsx
+++ client/src/pages/Login.tsx
@@ -12,17 +12,21 @@ function Login() {
           <aside className="flex h-full flex-1 items-center justify-center bg-login bg-cover bg-center dark:bg-loginDark"></aside>
         )}
         <article className="flex h-full flex-1 flex-col items-center justify-center bg-sky-100 p-7 dark:bg-zinc-800">
-          <h1 className="mb-10 text-2xl dark:text-white">Sign in</h1>
+          <article className="flex items-center justify-center gap-5 mb-16">
+            <img className="h-[40px]" src="images/icons/icon.png" alt="icon" />
+            <h1 className="text-3xl dark:text-white">Tweeter</h1>
+          </article>
+          <h2 className="mb-5 w-full text-xl dark:text-white sm:max-w-[75%]">Sign in</h2>
           <form action="" className="flex w-full flex-col gap-4 sm:max-w-[75%] sm:p-0">
             <input
-              className="w-full rounded-md border-2 p-2"
+              className="w-full rounded-md border p-2 text-white dark:border-gray-700 dark:bg-transparent"
               type="text"
               name="email"
               id="email"
               placeholder="Email"
             />
             <input
-              className="w-full rounded-md border-2 p-2"
+              className="w-full rounded-md border p-2 text-white dark:border-gray-700 dark:bg-transparent"
               type="password"
               name="password"
               id="password"
@@ -38,12 +42,12 @@ function Login() {
             </p>
           </form>
           <div className="relative inline-flex w-full items-center justify-center">
-            <hr className="my-8 h-px w-full border-0 bg-zinc-400 dark:bg-gray-700 sm:max-w-[75%]" />
+            <hr className="my-12 h-px w-full border-0 bg-zinc-400 dark:bg-gray-700 sm:max-w-[75%]" />
             <span className="absolute left-1/2 -translate-x-1/2 bg-sky-100 px-3 font-medium text-zinc-400 dark:bg-zinc-800 dark:text-white">
               OR
             </span>
           </div>
-          <article className="flex w-full cursor-pointer place-items-center justify-center gap-4 rounded-md border border-black py-2 px-4 hover:bg-sky-700 hover:text-white dark:border-gray-700 sm:max-w-[75%]">
+          <article className="flex w-full cursor-pointer place-items-center justify-center gap-4 rounded-md border border-zinc-400 py-2 px-4 hover:bg-sky-700 hover:text-white dark:border-gray-700 sm:max-w-[75%]">
             <img className="w-10" src="images/icons/Google.svg" alt="google" />
             <span className="font-semibold dark:text-white">Sign in with Google</span>
           </article>
