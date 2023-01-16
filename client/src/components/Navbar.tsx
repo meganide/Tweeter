ebb449a4f09fd33469715cd48154d626168860af
Meganide <renashassan@hotmail.com>
2023-01-16 08:38:06 +0100

    added navbar

--- client/src/components/Navbar.tsx
+++ client/src/components/Navbar.tsx
@@ -1,8 +1,48 @@
+import { Link, useLocation } from 'react-router-dom';
+import MediaQuery from 'react-responsive';
 
 function Navbar() {
+  const location = useLocation();
+
   return (
-    <div>Navbar</div>
-  )
+    <section className="flex h-[60px] items-center justify-center p-1 px-3 shadow-[0_2px_2px_rgba(0,0,0,0.05)]">
+      <section className="flex w-full max-w-7xl items-center justify-between">
+        <Link to="/">
+          <img className="w-[40px] cursor-pointer" src="images/icons/icon.png" alt="icon" />
+        </Link>
+        <MediaQuery minWidth={768}>
+          <Link
+            className={`font-[poppins] font-semibold text-[#828282] ${
+              location.pathname === '/' &&
+              'underline decoration-accent decoration-2 underline-offset-8'
+            }`}
+            to="/"
+          >
+            Home
+          </Link>
+          <Link
+            className={`font-[poppins] font-semibold text-[#828282] ${
+              location.pathname === '/explore' &&
+              'underline decoration-accent decoration-2 underline-offset-8'
+            }`}
+            to="/explore"
+          >
+            Explore
+          </Link>
+          <Link
+            className={`font-[poppins] font-semibold text-[#828282] ${
+              location.pathname === '/bookmarks' &&
+              'underline decoration-accent decoration-2 underline-offset-8'
+            }`}
+            to="/bookmarks"
+          >
+            Bookmarks
+          </Link>
+        </MediaQuery>
+        <img className="w-[40px] cursor-pointer" src="images/icons/avatar.png" alt="avatar" />
+      </section>
+    </section>
+  );
 }
 
-export default Navbar
+export default Navbar;
--- client/src/pages/ErrorPage.tsx
+++ client/src/pages/ErrorPage.tsx
@@ -1,16 +1,20 @@
 import { useRouteError } from 'react-router-dom';
+import Navbar from '../components/Navbar';
 
 function ErrorPage() {
   const error: any = useRouteError();
   console.error(error);
 
   return (
-    <div className="flex min-h-screen w-screen flex-col items-center justify-center">
-      <h1 className="mb-1 text-2xl">Oops!</h1>
-      <p className="mb-1">Sorry, an unexpected error has occured.</p>
-      <p>
-        <i>{error.statusText || error.messsage}</i>
-      </p>
+    <div className="flex flex-col min-h-screen w-screen">
+      <Navbar />
+      <div className="flex flex-col items-center justify-center flex-grow">
+        <h1 className="mb-1 text-2xl">Oops!</h1>
+        <p className="mb-1">Sorry, an unexpected error has occured.</p>
+        <p>
+          <i>{error.statusText || error.messsage}</i>
+        </p>
+      </div>
     </div>
   );
 }
