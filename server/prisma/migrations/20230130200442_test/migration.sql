ee9e4be4323b83393da5c98446d3571030b7251d
Meganide <renashassan@hotmail.com>
2023-02-02 21:03:10 +0100

    changed build command

--- server/prisma/migrations/20230130200442_test/migration.sql
+++
@@ -1,2 +0,0 @@
--- AlterTable
-ALTER TABLE `user` ADD COLUMN `profilePic` VARCHAR(191) NULL DEFAULT 'https://variety.com/wp-content/uploads/2022/11/Screen-Shot-2022-11-02-at-8.33.52-AM.png?w=681&h=383&crop=1';
--- server/prisma/migrations/20230130203709_added_default_profile/migration.sql
+++
@@ -1,3 +0,0 @@
--- AlterTable
-ALTER TABLE `profile` MODIFY `bio` VARCHAR(191) NULL DEFAULT 'No bio assigned yet...',
-    MODIFY `backgroundImg` VARCHAR(191) NULL DEFAULT 'http://wallpaperset.com/w/full/9/3/b/224224.jpg';
