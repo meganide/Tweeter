07da36a0651d23108865211cd176942fabbdf052
Meganide <renashassan@hotmail.com>
2023-02-02 20:24:18 +0100

    added render to origin on cors

--- server/src/app.ts
+++ server/src/app.ts
@@ -19,7 +19,7 @@ app.use(allowShareCookies);
 app.use(express.json({ limit: '10mb' }));
 app.use(
   cors({
-    origin: 'http://localhost:5173',
+    origin: ['https://tweeter-ps0r.onrender.com', 'http://localhost:5173'],
   })
 );
 app.use(cookieParser());
