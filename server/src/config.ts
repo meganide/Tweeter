0607006978d2b35cd3156e163f3756b8c1517cd8
Meganide <renashassan@hotmail.com>
2023-02-02 19:47:39 +0100

    aws setup

--- server/src/config.ts
+++ server/src/config.ts
@@ -7,6 +7,8 @@ const config = {
   CLOUDINARY_NAME: process.env.CLOUDINARY_NAME,
   CLOUDINARY_API_KEY: process.env.CLOUDINARY_API_KEY,
   CLOUDINARY_API_SECRET: process.env.CLOUDINARY_API_SECRET,
+  DB_AWS_PASSWORD: process.env.DB_AWS_PASSWORD,
+  DB_AWS_HOST: process.env.DB_AWS_HOST,
 };
 
 export { config };
