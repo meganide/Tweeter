dbd349d6343ed406b316eae0f1c62756f69102fb
Meganide <renashassan@hotmail.com>
2023-02-03 11:31:09 +0100

    added error msg for posting tweets and comments

--- client/src/features/Post/PostFooter.tsx
+++ client/src/features/Post/PostFooter.tsx
@@ -15,6 +15,7 @@ function PostFooter(props: IProps) {
   const { postData } = props;
 
   const [reply, setReply] = useState('');
+  const [error, setError] = useState('');
   const queryClient = useQueryClient();
 
   const mutation = useMutation(
@@ -31,6 +32,10 @@ function PostFooter(props: IProps) {
 
   async function submitReply() {
     try {
+      setError('');
+      if (reply.length < 1) {
+        return setError("Reply field can't be empty!");
+      }
       await mutation.mutate({ reply, postId: postData.id });
       setReply('');
     } catch (err) {
@@ -39,26 +44,24 @@ function PostFooter(props: IProps) {
   }
 
   return (
-    <section className="flex gap-2">
-      <Avatar />
-      <article className="flex w-full place-items-center gap-2 rounded-lg border bg-main-default p-2 text-sm dark:border-neutral-800 dark:bg-main-dark">
-        <input
-          className="mr-3 h-full w-full bg-transparent focus:outline-none dark:text-neutral-400 dark:placeholder:text-neutral-600"
-          type="text"
-          name="reply"
-          id="reply"
-          placeholder="Tweet your reply"
-          value={reply}
-          onChange={(e) => setReply(e.target.value)}
-        />
-        <ImageOutlinedIcon className="cursor-pointer text-gray-400 dark:text-neutral-500" />
-        <Button
-          type="button"
-          text="Reply"
-          styles="text-xs lg:text-base px-2"
-          onClick={submitReply}
-        />
-      </article>
+    <section>
+      <section className="flex gap-2">
+        <Avatar />
+        <article className="flex w-full place-items-center gap-2 rounded-lg border bg-main-default p-2 text-sm dark:border-neutral-800 dark:bg-main-dark">
+          <input
+            className="mr-3 h-full w-full bg-transparent focus:outline-none dark:text-neutral-400 dark:placeholder:text-neutral-600"
+            type="text"
+            name="reply"
+            id="reply"
+            placeholder="Tweet your reply"
+            value={reply}
+            onChange={(e) => setReply(e.target.value)}
+          />
+          <ImageOutlinedIcon className="cursor-pointer text-gray-400 dark:text-neutral-500" />
+          <Button type="button" text="Reply" styles="text-xs lg:text-base px-2" onClick={submitReply} />
+        </article>
+      </section>
+      {error && <p className="mt-2 ml-[64px] text-sm text-red-600">{error}</p>}
     </section>
   );
 }
