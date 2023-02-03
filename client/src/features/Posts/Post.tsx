3760b7e49536b01c2502927633a3f61556246d75
Meganide <renashassan@hotmail.com>
2023-02-03 07:46:33 +0100

    done

--- client/src/features/Posts/Post.tsx
+++ client/src/features/Posts/Post.tsx
@@ -102,7 +102,7 @@ function PostInfo(props: IProps) {
 
   return (
     <section className="mt-5 flex justify-end gap-4 text-xs text-gray-400 dark:text-neutral-500 sm:text-base">
-      <p className="cursor-pointer">{postData.comments.length} Comments</p>
+      <p className="cursor-pointer">{postData?.comments?.length} Comments</p>
       <p className="cursor-pointer">4 Retweets</p>
       <p className="cursor-pointer">7 Saved</p>
     </section>
