5a7499efa2ed5cccc1403b91b04c9bb8c94b08a9
Meganide <renashassan@hotmail.com>
2023-02-06 21:41:33 +0100

    styled custom scrollbar

--- client/tailwind.config.cjs
+++ client/tailwind.config.cjs
@@ -10,24 +10,20 @@ module.exports = {
           dark: '#121212',
         },
         secondary: {
-          dark: '#1D1D1D'
+          dark: '#1D1D1D',
         },
         border: {
-          dark: 'rgb(37,37,37)'
+          dark: 'rgb(37,37,37)',
         },
         accent: '#2F80ED',
       },
       backgroundImage: {
-        login:
-          'linear-gradient(to right, rgba(255,255,255,0.1), rgba(224, 242, 254, 1)),url("/images/login.png")',
-        loginDark:
-          'linear-gradient(to right, rgba(0,0,0,0.1), rgba(39, 39, 42, 1)),url("/images/login.png")',
-        register:
-          'linear-gradient(to left, rgba(255,255,255,0.1), rgba(224, 242, 254, 1)),url("/images/register.png")',
-        registerDark:
-          'linear-gradient(to left, rgba(0,0,0,0.1), rgba(39, 39, 42, 1)),url("/images/register.png")',
+        login: 'linear-gradient(to right, rgba(255,255,255,0.1), rgba(224, 242, 254, 1)),url("/images/login.png")',
+        loginDark: 'linear-gradient(to right, rgba(0,0,0,0.1), rgba(39, 39, 42, 1)),url("/images/login.png")',
+        register: 'linear-gradient(to left, rgba(255,255,255,0.1), rgba(224, 242, 254, 1)),url("/images/register.png")',
+        registerDark: 'linear-gradient(to left, rgba(0,0,0,0.1), rgba(39, 39, 42, 1)),url("/images/register.png")',
       },
     },
   },
-  plugins: [],
+  plugins: [require('tailwind-scrollbar')],
 };
