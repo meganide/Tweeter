5b2e8e8aa8bd47679b3acada43a96f9a8aa48426
Meganide <renashassan@hotmail.com>
2023-02-07 15:42:48 +0100

    fixed position of edit photo

--- client/src/features/Profile/User/UserCard.tsx
+++ client/src/features/Profile/User/UserCard.tsx
@@ -1,8 +1,9 @@
+import { useContext } from 'react';
 import { useMediaQuery } from 'react-responsive';
 
 import Avatar from '../../../components/common/Avatar';
 import Card from '../../../components/common/Card';
-import { ICurrentUser } from '../../../contexts/authContext';
+import { AuthContext, IAuthContext, ICurrentUser } from '../../../contexts/authContext';
 import { useUpload } from '../../../hooks/useUpload';
 import UserInfo from './UserInfo';
 
@@ -14,6 +15,8 @@ interface IProps {
 
 function UserCard(props: IProps) {
   const { userProfile } = props;
+
+  const { currentUser } = useContext(AuthContext) as IAuthContext;
 
   const isBigScreen = useMediaQuery({ query: '(min-width: 1024px)' });
 
@@ -35,21 +38,22 @@ function UserCard(props: IProps) {
   return (
     <section>
       <input className="hidden" type="file" name="profilePic" accept=".jpg, .jpeg, .png" ref={inputFileRefBg} onChange={handleFileInputChangeBg} />
-      <section className="relative">
-        <img
-          className="max-h-[200px] md:max-h-[320px] w-full object-cover object-center"
-          src={previewBgImage ? previewBgImage : userProfile?.profile?.backgroundImg}
-          alt="background"
-        />
-        <CameraAltIcon
-          className="absolute right-3 -bottom-3 cursor-pointer rounded-[50%] bg-neutral-600 p-[6px] text-white"
-          style={{ fontSize: '2.3rem' }}
-          onClick={ChooseBgImage}
-        >
-          Change
-        </CameraAltIcon>
-      </section>
-      <section className='-translate-y-8" m-auto max-w-7xl px-3 pb-4 lg:px-3 xl:px-0'>
+      <img
+        className="max-h-[200px] w-full object-cover object-center md:max-h-[320px]"
+        src={previewBgImage ? previewBgImage : userProfile?.profile?.backgroundImg}
+        alt="background"
+      />
+
+      <section className='-translate-y-8" relative m-auto max-w-7xl px-3 pb-4 lg:px-3 xl:px-0'>
+        {currentUser?.name === userProfile.name && (
+          <CameraAltIcon
+            className="absolute right-3 -top-5 cursor-pointer rounded-[50%] bg-neutral-600 p-[6px] text-white"
+            style={{ fontSize: '2.3rem' }}
+            onClick={ChooseBgImage}
+          >
+            Change
+          </CameraAltIcon>
+        )}
         <Card>
           <section className="flex flex-col place-items-center gap-5 p-4 pb-0 lg:flex-row lg:p-10 lg:pb-0">
             <section className="shrink-0 -translate-y-[50%] place-items-start rounded-lg border-2 border-white shadow-lg dark:border-secondary-dark dark:shadow-sm dark:shadow-gray-500">
@@ -63,13 +67,15 @@ function UserCard(props: IProps) {
               />
               <section className="relative">
                 <Avatar height={imgHeight} imgSrc={previewImage ? previewImage : userProfile?.profilePic} />
-                <CameraAltIcon
-                  className="absolute -right-3 -bottom-3 cursor-pointer rounded-[50%] bg-neutral-600 p-[6px] text-white"
-                  style={{ fontSize: '2.3rem' }}
-                  onClick={chooseImage}
-                >
-                  Change
-                </CameraAltIcon>
+                {currentUser?.name === userProfile.name && (
+                  <CameraAltIcon
+                    className="absolute -right-3 -bottom-3 cursor-pointer rounded-[50%] bg-neutral-600 p-[6px] text-white"
+                    style={{ fontSize: '2.3rem' }}
+                    onClick={chooseImage}
+                  >
+                    Change
+                  </CameraAltIcon>
+                )}
               </section>
             </section>
             <UserInfo userProfile={userProfile} />
