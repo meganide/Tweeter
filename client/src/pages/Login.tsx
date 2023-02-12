376d32dd59fb7c365b9f13bc6709bd20f7fdf3c0
Meganide <renashassan@hotmail.com>
2023-02-13 00:55:29 +0100

    removed google login from ui

--- client/src/pages/Login.tsx
+++ client/src/pages/Login.tsx
@@ -1,7 +1,5 @@
-import HorizontalLine from '../components/common/HorizontalLine';
 import LoginHeader from '../features/Login/LoginHeader';
 import LoginInputs from '../features/Login/LoginInputs';
-import SocialLogins from '../components/common/SocialLogins';
 import { useMediaQuery } from 'react-responsive';
 
 function Login() {
@@ -13,12 +11,10 @@ function Login() {
     <section className="flex min-h-screen items-center justify-center bg-slate-100 p-3 dark:bg-main-dark">
       <section className="flex h-full max-h-[80%] w-full max-w-7xl overflow-hidden rounded-lg">
         {isDesktopOrLaptop && <aside className="flex flex-1 items-center justify-center bg-login bg-cover bg-center dark:bg-loginDark"></aside>}
-        <article className="flex h-full flex-1 flex-col items-center justify-center bg-sky-100 py-16 px-4 dark:bg-zinc-800">
+        <article className="flex h-full flex-1 flex-col items-center justify-center bg-sky-100 py-32 px-4 dark:bg-zinc-800">
           <LoginHeader />
           <h2 className="mb-5 w-full text-xl dark:text-white sm:max-w-[75%]">Sign in</h2>
           <LoginInputs />
-          <HorizontalLine />
-          <SocialLogins />
         </article>
       </section>
     </section>
--- client/src/pages/Register.tsx
+++ client/src/pages/Register.tsx
@@ -1,7 +1,5 @@
-import HorizontalLine from '../components/common/HorizontalLine';
 import RegisterHeader from '../features/Register/RegisterHeader';
 import RegisterInputs from '../features/Register/RegisterInputs';
-import SocialLogins from '../components/common/SocialLogins';
 import { useMediaQuery } from 'react-responsive';
 
 function Register() {
@@ -12,12 +10,10 @@ function Register() {
   return (
     <section className="flex min-h-screen items-center justify-center bg-slate-100 p-3 dark:bg-main-dark">
       <section className="flex h-full max-h-[80%] w-full max-w-7xl overflow-hidden rounded-lg">
-        <article className="flex h-full flex-1 flex-col items-center justify-center bg-sky-100 py-16 px-4 dark:bg-zinc-800">
+        <article className="flex h-full flex-1 flex-col items-center justify-center bg-sky-100 py-32 px-4 dark:bg-zinc-800">
           <RegisterHeader />
           <h2 className="mb-5 w-full text-xl dark:text-white sm:max-w-[75%]">Register</h2>
           <RegisterInputs />
-          <HorizontalLine />
-          <SocialLogins />
         </article>
         {isDesktopOrLaptop && <aside className="flex flex-1 items-center justify-center bg-register bg-cover bg-center dark:bg-registerDark"></aside>}
       </section>
